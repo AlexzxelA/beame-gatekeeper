6e29286a890fec12573fa0ac058d26ea0a1c9865
zenit1 <serge@beame.io>
2016-11-10 14:11:01 +0200

    init services completed

--- defaults.js
+++ defaults.js
@@ -7,6 +7,11 @@ const path = require('path');
 const os   = require('os');
 const home = os.homedir();
 
+
+//DON'T change these settings
+const beame_server_folder_name = ".beame_server";
+const beame_server_folder_path = path.join(home, beame_server_folder_name);
+
 const DbProviders = {
 	"Sqlite":    "sqlite",
 	"Couchbase": "couchbase"
@@ -16,19 +21,31 @@ const db_provider = DbProviders.Sqlite;
 
 const sqlite_db_name           = "beame_server.db";
 const sqlite_db_admin_username = "admin";
-const sqlite_db_storage_root   = path.join(__dirname, "data");
+const sqlite_env_name          = "production";
+const sqlite_db_storage_root   = path.join(home, ".beame_data");
 
-const beame_server_folder_name = ".beame_server";
-const beame_server_folder_path = path.join(home, beame_server_folder_name);
+const SqliteConfigTemplate = {
+	[sqlite_env_name] : {
+		"username":             "",
+		"password":             "",
+		"storage":              "",
+		"database":             "beame_server",
+		"host":                 "127.0.0.1",
+		"dialect":              "sqlite",
+		"autoMigrateOldSchema": true
+	}
+};
 
 const ConfigProps = {
 	Settings: {
 		DbProvider: "db_provider"
 	},
 	Sqlite:   {
-		DbName:        "sqlite_db_name",
-		AdminUserName: "sqlite_db_admin_username",
-		StorageRoot:   "sqlite_db_storage_root"
+		ConfigTemplate: "SqliteConfigTemplate",
+		DbName:         "sqlite_db_name",
+		AdminUserName:  "sqlite_db_admin_username",
+		StorageRoot:    "sqlite_db_storage_root",
+		EnvName:        "sqlite_env_name"
 	},
 	Servers:  {
 		GatewayFqdn:   "gateway_server_fqdn",
@@ -41,19 +58,20 @@ const ConfigProps = {
 		BeameFolderRootPath: "beame_server_folder_path",
 		BeameFolderName:     "beame_server_folder_name"
 	}
-
-
 };
 
 module.exports = {
 	ConfigProps,
 	DbProviders,
 
+	SqliteConfigTemplate,
+
 	db_provider,
 
 	sqlite_db_name,
 	sqlite_db_storage_root,
 	sqlite_db_admin_username,
+	sqlite_env_name,
 
 	beame_server_folder_path,
 	beame_server_folder_name,
--- src/bootstrapper.js
+++ src/bootstrapper.js
@@ -3,9 +3,16 @@
  */
 
 const path = require('path');
+// const os   = require('os');
+// const home = os.homedir();
 
-
-const execFile = require('child_process').execFile;
+const defaults      = require('../defaults');
+//const BeameDirProps = defaults.ConfigProps.BeameDir;
+const SqliteProps   = defaults.ConfigProps.Sqlite;
+//const ServersProps  = defaults.ConfigProps.Servers;
+const SettingsProps = defaults.ConfigProps.Settings;
+const DbProviders   = defaults.DbProviders;
+const execFile      = require('child_process').execFile;
 
 
 const beameSDK    = require('beame-sdk');
@@ -18,145 +25,247 @@ const CommonUtils = beameSDK.CommonUtils;
 const DirectoryServices = beameSDK.DirectoryServices;
 const dirServices       = new DirectoryServices();
 
-const ConfigJsonPath       = path.join(__dirname, "..", "config", "config.json");
-const SqliteConfigJsonPath = path.join(__dirname, "..", "config", "db_config.json");
+const AppConfigFileName      = "app_config.json";
+const SqliteDbConfigFileName = "db_config.json";
+const BeameRootPath          = defaults.beame_server_folder_path;
+const ConfigFolderPath       = path.join(BeameRootPath, "config");
+const CredsFolderPath        = path.join(BeameRootPath, "creds");
+const ConfigJsonPath         = path.join(BeameRootPath, "config", AppConfigFileName);
+const SqliteConfigJsonPath   = path.join(BeameRootPath, "config", SqliteDbConfigFileName);
 
-const defaults      = require('../defaults');
-const BeameDirProps = defaults.ConfigProps.BeameDir;
-const SqliteProps   = defaults.ConfigProps.Sqlite;
-//const ServersProps  = defaults.ConfigProps.Servers;
-const SettingsProps = defaults.ConfigProps.Settings;
-const DbProviders   = defaults.DbProviders;
+const __onConfigError = error=> {
+	logger.error(error);
+	process.exit(1);
+};
 
 class Bootstrapper {
 
 	constructor() {
-		this._config = null;
+		let config   = DirectoryServices.readJSON(ConfigJsonPath);
+		this._config = CommonUtils.isObjectEmpty(config) ? null : config;
 	}
 
-	ensureConfiguration() {
-
-		const __onConfigError = error=> {
-			logger.error(error);
-			process.exit(1);
-		};
-
-		return this._ensureConfigJson().then(this._ensureDb.bind(this)).then(this._ensureBeameServerDir.bind(this)).catch(__onConfigError);
-
+	initAll(){
+		this.initConfig().then(this.initDb.bind(this)).then(()=>{
+			logger.info(`beame-insta-server bootstrapped successfully`);
+			process.exit(0);
+		}).catch(__onConfigError);
 	}
 
+	initConfig() {
 
-	_ensureConfigJson() {
-
-		logger.debug(`ensuring config.json...`);
-
-		let isExists = DirectoryServices.doesPathExists(ConfigJsonPath);
-
-		if (isExists) {
-			logger.debug(`config.json found...`);
-			return this._updateConfigFile();
-		}
-
-		logger.debug(`creating config.json...`);
-		let config = Bootstrapper._buildConfigJson();
-
-		this._config = config;
-
-		return dirServices.saveFileAsync(ConfigJsonPath, CommonUtils.stringify(config, false)).then(()=> {
-			logger.debug(`config.json saved...`);
-			return Promise.resolve();
-		}).catch(error=> {
-			this._config = null;
-			return Promise.reject(error);
-		});
-	}
-
-	static _buildConfigJson() {
-		let config = {};
-
-		for (let prop in defaults) {
-			//noinspection JSUnfilteredForInLoop
-			if (typeof defaults[prop] === "string") {
-				//noinspection JSUnfilteredForInLoop
-				config[prop] = defaults[prop];
+		return new Promise((resolve) => {
+			Bootstrapper._ensureBeameServerDir().then(this._ensureAppConfigJson.bind(this)).then(this._ensureDbConfig.bind(this)).then(resolve).catch(__onConfigError)
 			}
+		);
 
-		}
 
-		return config;
 	}
 
-	_updateConfigFile() {
-		try {
-			let config     = DirectoryServices.readJSON(ConfigJsonPath),
-			    updateFile = false;
+	initDb() {
+		return new Promise((resolve, reject) => {
+				let provider = this._config[SettingsProps.DbProvider];
 
-			if (CommonUtils.isObjectEmpty(config)) {
-				return Promise.reject(`config json file corrupted`);
+				logger.debug(`DB Provider set to ${provider}...`);
+
+				if (!provider) {
+					reject(`Db Provider not defined`);
+					return;
+				}
+
+				switch (provider) {
+					case DbProviders.Sqlite:
+						this._ensureSqliteDir().then(this._migrateSqliteSchema.bind(this)).then(resolve).catch(__onConfigError);
+						return;
+					//TODO implement Couchbase connector
+					// case DbProviders.Couchbase:
+					// 	break;
+				}
+
+				reject(`Db Provider ${provider} currently not supported`);
 			}
+		);
+	}
 
-			for (let prop in defaults) {
-				//noinspection JSUnfilteredForInLoop
-				if (typeof defaults[prop] === "string" && !config.hasOwnProperty(prop)) {
-					updateFile   = true;
+	//region Beame folder
+	static _ensureBeameServerDir() {
+
+		return Bootstrapper._ensureDir(BeameRootPath).then(Bootstrapper._ensureDir(ConfigFolderPath)).then(Bootstrapper._ensureDir(CredsFolderPath));
+	}
+	//endregion
+
+	//region App Config
+	_ensureAppConfigJson() {
+
+		return new Promise((resolve) => {
+				logger.debug(`ensuring ${AppConfigFileName}...`);
+
+				let isExists = DirectoryServices.doesPathExists(ConfigJsonPath);
+
+				if (isExists) {
+					logger.debug(`${AppConfigFileName} found...`);
+					this._updateAppConfigJson().then(resolve).catch(__onConfigError);
+				}
+				else {
+					this._createAppConfigJson().then(resolve).catch(__onConfigError);
+				}
+
+			}
+		);
+
+
+	}
+
+	_createAppConfigJson() {
+
+		return new Promise((resolve, reject) => {
+				logger.debug(`creating ${AppConfigFileName}...`);
+
+				let config = {};
+
+				for (let prop in defaults) {
 					//noinspection JSUnfilteredForInLoop
-					config[prop] = defaults[prop];
+					if (typeof defaults[prop] === "string") {
+						//noinspection JSUnfilteredForInLoop
+						config[prop] = defaults[prop];
+					}
+				}
+
+				this._config = config;
+
+				dirServices.saveFileAsync(ConfigJsonPath, CommonUtils.stringify(config)).then(()=> {
+					logger.debug(`${AppConfigFileName} saved in ${path.dirname(ConfigJsonPath)}...`);
+					resolve();
+				}).catch(error=> {
+					this._config = null;
+					reject(error);
+				});
+			}
+		);
+	}
+
+	_updateAppConfigJson() {
+
+		return new Promise((resolve, reject) => {
+				try {
+					let config     = DirectoryServices.readJSON(ConfigJsonPath),
+					    updateFile = false;
+
+					if (CommonUtils.isObjectEmpty(config)) {
+						reject(`${AppConfigFileName} file corrupted`);
+						return;
+					}
+
+					for (let prop in defaults) {
+						//noinspection JSUnfilteredForInLoop
+						if (typeof defaults[prop] === "string" && !config.hasOwnProperty(prop)) {
+							updateFile   = true;
+							//noinspection JSUnfilteredForInLoop
+							config[prop] = defaults[prop];
+						}
+					}
+
+					this._config = config;
+
+					if (!updateFile) {
+						logger.debug(`no changes found for ${AppConfigFileName}...`);
+						resolve();
+						return;
+					}
+
+					dirServices.saveFileAsync(ConfigJsonPath, CommonUtils.stringify(config, false)).then(()=> {
+						logger.debug(`${AppConfigFileName} updated...`);
+						resolve();
+					}).catch(error=> {
+						this._config = null;
+						reject(error);
+					});
+
+				} catch (error) {
+					reject(error);
 				}
 			}
+		);
 
-			this._config = config;
 
-			if (!updateFile) {
-				logger.debug(`no changes found for config.json...`);
-				return Promise.resolve();
+	}
+
+	//endregion
+
+	//region Db services
+	//region config
+	_ensureDbConfig() {
+
+		return new Promise((resolve, reject) => {
+				let provider = this._config[SettingsProps.DbProvider];
+
+				logger.debug(`DB Provider set to ${provider}...`);
+
+				if (!provider) {
+					reject(`Db Provider not defined`);
+					return;
+				}
+
+				switch (provider) {
+					case DbProviders.Sqlite:
+						this._ensureSqliteConfigJson().then(resolve).catch(__onConfigError);
+						return;
+					//TODO implement Couchbase connector
+					// case DbProviders.Couchbase:
+					// 	break;
+				}
+
+				reject(`Db Provider ${provider} currently not supported`);
 			}
-
-			return dirServices.saveFileAsync(ConfigJsonPath, CommonUtils.stringify(config, false)).then(()=> {
-				logger.debug(`config.json updated...`);
-				return Promise.resolve();
-			}).catch(error=> {
-				this._config = null;
-				return Promise.reject(error);
-			});
-
-		} catch (e) {
-			return Promise.reject(e);
-		}
+		);
 	}
 
-	_ensureBeameServerDir() {
+	_ensureSqliteConfigJson() {
 
-		return this._ensureDir(BeameDirProps.BeameFolderRootPath);
+		return new Promise((resolve) => {
+
+				logger.debug(`validating sqlite ${SqliteDbConfigFileName}...`);
+
+				let isExists = DirectoryServices.doesPathExists(SqliteConfigJsonPath);
+
+				if (isExists) {
+					logger.debug(`sqlite ${SqliteDbConfigFileName} found...`);
+					resolve();
+				}
+				else {
+					this._createSqliteConfigJson().then(resolve).catch(__onConfigError);
+				}
+
+			}
+		);
+
 	}
 
-	_ensureDb() {
-		let provider = this._config[SettingsProps.DbProvider];
+	_createSqliteConfigJson() {
 
-		logger.debug(`DB Provider set to ${provider}...`);
+		return new Promise((resolve, reject) => {
+				let dbConfig = defaults.SqliteConfigTemplate,
+				    env      = this._config[SqliteProps.EnvName];
 
-		if (!provider) {
-			return Promise.reject(`Db Provider not defined`);
-		}
+				dbConfig[env]["username"] = this._config[SqliteProps.AdminUserName];
+				dbConfig[env]["password"] = CommonUtils.randomPassword(12);
+				dbConfig[env]["storage"]  = path.join(this._config[SqliteProps.StorageRoot], this._config[SqliteProps.DbName]);
 
-		switch (provider) {
-			case DbProviders.Sqlite:
-				return this.ensureSqlite();
-			//TODO implement Couchbase connector
-			// case DbProviders.Couchbase:
-			// 	break;
-		}
+				dirServices.saveFileAsync(SqliteConfigJsonPath, CommonUtils.stringify(dbConfig)).then(()=> {
+					logger.debug(`${SqliteDbConfigFileName} saved in ${path.dirname(SqliteConfigJsonPath)}...`);
+					resolve();
+				}).catch(error=> {
+					reject(error);
+				});
 
-		return Promise.reject(`Db Provider ${provider} currently not supported`);
+			}
+		);
+
+
 	}
+	//endregion
 
-	ensureSqlite() {
-
-		logger.debug(`ensuring sqlite...`);
-
-		return this._ensureSqliteDir().then(this._ensureSqliteConfigJson.bind(this)).then(this._migrateSqliteSchema.bind(this)).catch(error=> {
-			return Promise.reject(error);
-		});
-	}
+	//region initConfig
 
 	_migrateSqliteSchema() {
 
@@ -165,7 +274,7 @@ class Bootstrapper {
 		return new Promise((resolve, reject) => {
 				//TODO implement https://github.com/sequelize/umzug
 				let action = path.join(__dirname, "..", "node_modules", ".bin", "sequelize"),
-				    args   = ["db:migrate", "--env", "production", "--config", path.join(__dirname, "..", "config", "db_config.json")];
+				    args   = ["db:migrate", "--env", "production", "--config", SqliteConfigJsonPath];
 
 				try {
 					execFile(action, args, function (error) {
@@ -186,62 +295,33 @@ class Bootstrapper {
 
 	_ensureSqliteDir() {
 
-		return this._ensureDir(SqliteProps.StorageRoot);
+		return this._ensureConfigDir(SqliteProps.StorageRoot);
 	}
+	//endregion
 
-	_ensureSqliteConfigJson() {
 
-		logger.debug(`validating sqlite db_config.json...`);
+	//endregion
 
-		let dbConfig   = DirectoryServices.readJSON(SqliteConfigJsonPath),
-		    updateFile = false;
-
-		if (CommonUtils.isObjectEmpty(dbConfig)) {
-			return Promise.reject(`sqlite db config.json file corrupted`);
-		}
-
-		let adminName = dbConfig["production"]["username"],
-		    adminPwd  = dbConfig["production"]["password"],
-		    storage   = dbConfig["production"]["storage"];
-
-		if (adminName === "") {
-			updateFile                         = true;
-			dbConfig["production"]["username"] = this._config[SqliteProps.AdminUserName];
-			logger.debug(`admin username set to ${this._config[SqliteProps.AdminUserName]}...`);
-		}
-
-		if (adminPwd === "") {
-			updateFile                         = true;
-			dbConfig["production"]["password"] = CommonUtils.randomPassword(12);
-			logger.debug(`admin password created...`)
-		}
-
-		if (storage === "") {
-			updateFile                        = true;
-			dbConfig["production"]["storage"] = path.join(this._config[SqliteProps.StorageRoot], this._config[SqliteProps.DbName]);
-			logger.debug(`sqlite storage path updated...`)
-		}
-
-		return updateFile ? dirServices.saveFileAsync(SqliteConfigJsonPath, CommonUtils.stringify(dbConfig, false)) : Promise.resolve();
-	}
-
-	_ensureDir(prop) {
+	//region Directory services
+	_ensureConfigDir(prop) {
 		let dir = this._config[prop];
 
 		if (!dir) {
 			return Promise.reject(`config.json not contains required "beame-server dir root path" value`);
 		}
 
+		return Bootstrapper._ensureDir(dir);
+	}
+
+	static _ensureDir(dir) {
 		DirectoryServices.createDir(dir);
 
-		logger.debug(`${dir} ensured...`);
+		logger.debug(`directory ${dir} ensured...`);
 
 		return Promise.resolve();
 	}
+	//endregion
 
-	// static _readEdgeMetadata() {
-	// 	return beameSDK.DirectoryServices.readJSON(beameUtils.makePath(config.rootDir, config.edgeMetadataFileName));
-	// }
 }
 
 module.exports = Bootstrapper;
