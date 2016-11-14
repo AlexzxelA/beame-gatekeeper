0cb7ac6ee259e55ec9cb466c917d7a65c5d6fb08
zenit1 <serge@beame.io>
2016-11-14 12:12:29 +0200

    register button href

--- tests/servers.js
+++ tests/servers.js
@@ -1,9 +1,12 @@
 /**
  * Created by zenit1 on 14/11/2016.
  */
+"use strict";
 const async =require('async');
 
 const Bootstrapper = require('../src/bootstrapper');
+const bootstrapper      = new Bootstrapper();
+
 const Constants    = require('../constants');
 
 const CustomerAuthServer = require('../src/servers/customer_auth/server');
@@ -11,6 +14,26 @@ const BeameAuthServer = require('../src/servers/beame_auth/server');
 
 
 async.parallel([
+	() =>{
+
+		const getServersSettings = bootstrapper.getServersSettings.bind(bootstrapper);
+		const ServersManager = require('../src/serversManager');
+
+		const assertServersSettings = (settings) => {
+			return new Promise((resolve, reject) => {
+				if(!settings) {
+					console.error('server settings error');
+					process.exit(1);
+				}
+				resolve(settings);
+			});
+		};
+
+		bootstrapper.initAll()
+			.then(getServersSettings)
+			.then(assertServersSettings)
+			.then(ServersManager.go);
+	},
 	() => {
 			let customer_auth_server = new CustomerAuthServer(Bootstrapper.getCredFqdn(Constants.CredentialType.GatewayServer));
 
