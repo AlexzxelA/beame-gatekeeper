ef22421b5cfef00a40f1e738c6ef05c3cbf01136
zenit1 <serge@beame.io>
2016-11-14 15:28:05 +0200

    sigin

---
+++ src/routers/gateway.js
@@ -0,0 +1,4 @@
+/**
+ * Created by zenit1 on 14/11/2016.
+ */
+
--- src/servers/gw/unauthenticatedApp.js
+++ src/servers/gw/unauthenticatedApp.js
@@ -1,18 +1,21 @@
-const path        = require('path');
+const path = require('path');
 
-const express     = require('express');
-const bodyParser  = require('body-parser');
+const express    = require('express');
+const bodyParser = require('body-parser');
 
-const utils       = require('../../utils');
-const cust_auth_app  = require('../../routers/customer_auth');
+const public_dir = path.join(__dirname, '..', '..', 'public');
+const base_path  = path.join(public_dir, 'pages', 'gw');
+
+const utils         = require('../../utils');
+const cust_auth_app = require('../../routers/customer_auth');
 
 const unauthenticatedApp = express();
 
 // TODO: refactor - start
 
-unauthenticatedApp.use(express.static(path.join(__dirname, '..', '..', '..', 'public', 'pages', 'gw'), {index: 'welcome.html'}));
+unauthenticatedApp.use(express.static(base_path), {index: 'welcome.html'});
 unauthenticatedApp.get('/signin', (req, res) => {
-	res.sendFile(path.join(__dirname, '..', '..', '..', 'public', 'pages', 'gw', 'signin.html'));
+	res.sendFile(path.join(base_path, 'signin.html'));
 });
 utils.setExpressAppCommonRoutes(unauthenticatedApp);
 
