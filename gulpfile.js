ccba77c93a1dfb8f4aa3de8eedac0f126b0dd58f
zenit1 <serge@beame.io>
2016-11-14 09:55:12 +0200

    sass on welcome page

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
 /**
  * Created by zenit1 on 13/11/2016.
  */
+"use strict";
+
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+	gulp.src('./public/scss/app.scss')
+		.pipe(sass().on('error', (err) => {
+			console.error(' gulp sass error ',err);
+			process.exit(1);
+		}))
+		.pipe(gulp.dest('./public/css/'));
+});
