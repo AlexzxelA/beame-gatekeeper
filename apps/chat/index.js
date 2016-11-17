98980ffa1038f529d2ac14366adc182664988cc0
Ilya Sher <ilya.sher@coding-knight.com>
2016-11-17 08:25:25 +0200

    [GW] Use chat app from SDK

--- apps/chat/index.js
+++ apps/chat/index.js
@@ -3,6 +3,8 @@
 const port = 65510;
 const host = 'localhost';
 
+// Hack attack!
+
 class Server {
 
 	start() {
@@ -12,20 +14,71 @@ class Server {
 		var http = require('http').Server(app);
 		var io = require('socket.io')(http);
 
-		app.get('/', function(req, res){
-			res.sendFile(__dirname + '/index.html');
+		app.get('/', function(req, res){ res.sendFile(__dirname + '/index.html');});
+		app.get('/main.js', function(req, res){ res.sendFile(__dirname + '/main.js');});
+		app.get('/style.css', function(req, res){ res.sendFile(__dirname + '/style.css');});
+
+		var numUsers = 0;
+
+		io.on('connection', function (socket) {
+			var addedUser = false;
+
+			// when the client emits 'new message', this listens and executes
+			socket.on('new message', function (data) {
+				// we tell the client to execute 'new message'
+				socket.broadcast.emit('new message', {
+					username: socket.username,
+					message: data
+				});
+			});
+
+			// when the client emits 'add user', this listens and executes
+			socket.on('add user', function (username) {
+				if (addedUser){ return;}
+
+				// we store the username in the socket session for this client
+				socket.username = username;
+				++numUsers;
+				addedUser = true;
+				socket.emit('login', {
+					numUsers: numUsers
+				});
+				// echo globally (all clients) that a person has connected
+				socket.broadcast.emit('user joined', {
+					username: socket.username,
+					numUsers: numUsers
+				});
+			});
+
+			// when the client emits 'typing', we broadcast it to others
+			socket.on('typing', function () {
+				socket.broadcast.emit('typing', {
+					username: socket.username
+				});
+			});
+
+			// when the client emits 'stop typing', we broadcast it to others
+			socket.on('stop typing', function () {
+				socket.broadcast.emit('stop typing', {
+					username: socket.username
+				});
+			});
+
+			// when the user disconnects.. perform this
+			socket.on('disconnect', function () {
+				if (addedUser) {
+					--numUsers;
+
+					// echo globally that this client has left
+					socket.broadcast.emit('user left', {
+						username: socket.username,
+						numUsers: numUsers
+					});
+				}
+			});
 		});
 
-		io.on('connection', function(socket){
-			console.log('a user connected');
-			socket.on('disconnect', function(){
-				console.log('user disconnected');
-			});
-			socket.on('chat message', function(msg){
-				console.log('message: ' + msg);
-				io.emit('chat message', msg);
-			});
-		});
+		
 
 		http.listen(port, host, function(){
 			console.log(`listening on ${host}:${port}`);
