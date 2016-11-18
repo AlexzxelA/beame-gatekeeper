ece1db1b93fe4865dfb2fd2515e69f77b63e472f
zenit1 <serge@beame.io>
2016-11-18 21:01:29 +0200

    deploy cdn included

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,26 @@ const strip        = require('gulp-strip-comments');
 const inlinesource = require('gulp-inline-source');
 const htmlmin      = require('gulp-htmlmin');
 const clean        = require('gulp-rimraf');
+const cloudfront   = require("gulp-cloudfront-invalidate");
+
 
 const dist_folder_name = 'dist';
+
+const compilePage = (pagePath, distPath) => {
+
+	gulp.src(pagePath)
+		.pipe(htmlreplace({
+			'css':            'https://cdn.beame.io/insta-server/css/app.min.css',
+			'js':             'https://cdn.beame.io/insta-server/js/app.min.js',
+			'lib':            'https://cdn.beame.io/insta-server/js/lib.min.js',
+			'signin-js-head': 'https://cdn.beame.io/insta-server/js/signin.min.js',
+			'signup-js-head': 'https://cdn.beame.io/insta-server/js/signup.min.js'
+		}))
+		.pipe(htmlmin({collapseWhitespace: true}))
+		.pipe(inlinesource())
+		.pipe(gulp.dest(distPath));
+
+};
 
 gulp.task('sass', function () {
 	gulp.src('./public/scss/app.scss')
@@ -34,39 +52,47 @@ gulp.task("watch", function () {
 	gulp.watch("./public/scss/**/*.scss", ["sass"]);
 });
 
-function handlePage(pagePath, distPath) {
 
-	gulp.src(pagePath)
-		.pipe(htmlreplace({
-			'css': 'css/app.min.css?v=' + new Date().getTime(),
-			'js':  'js/app.min.js?v=' + new Date().getTime()
-		}))
-		.pipe(htmlmin({collapseWhitespace: true}))
-		.pipe(inlinesource())
-		.pipe(gulp.dest(distPath));
-
-}
-
-gulp.task('clean', function () {
+gulp.task('clean', () => {
 	return gulp.src(`${dist_folder_name}`, {read: false})
 		.pipe(clean());
 });
 
-gulp.task('compile_css', function () {
+gulp.task('compile-css', () => {
 	gulp.src(['./public/css/app.css'])
 		.pipe(concat('app.min.css'))
 		.pipe(cleanCSS({compatibility: 'ie10'}))
 		.pipe(gulp.dest(`./${dist_folder_name}/css/`));
 });
 
-
-gulp.task('compile-production', ['sass', 'compile_css'], function () {
+gulp.task('compile-js', () => {
 
 	gulp.src(
 		[
-			'./public/lib/jquery/jquery.min.js',
-			'./public/lib/angular/angular.min.js',
-			'./public/lib/kendo/kendo.qr.min.js',
+			'./public/js/utils.js',
+			'./public/js/signin.js'
+		]
+		)
+		.pipe(concat('signin.min.js'))
+		.pipe(uglify())
+		.pipe(strip())
+		.pipe(stripDebug())
+		.pipe(gulp.dest(`./${dist_folder_name}/js/`));
+
+	gulp.src(
+		[
+			'./public/js/utils.js',
+			'./public/js/signup.js'
+		]
+		)
+		.pipe(concat('signup.min.js'))
+		.pipe(uglify())
+		.pipe(strip())
+		.pipe(stripDebug())
+		.pipe(gulp.dest(`./${dist_folder_name}/js/`));
+
+	gulp.src(
+		[
 			'./public/js/crypto.js',
 			'./public/js/session_controller.js',
 			'./public/js/qr.js',
@@ -79,19 +105,88 @@ gulp.task('compile-production', ['sass', 'compile_css'], function () {
 		.pipe(stripDebug())
 		.pipe(gulp.dest(`./${dist_folder_name}/js/`));
 
+	gulp.src(
+		[
+			'./public/lib/socket.io-1.4.5.min.js',
+			'./public/lib/angular-1.5.7.min.js',
+			'./public/lib/jquery-2.2.4.min.js',
+			'./public/lib/kendo-2016.3.1118.qr.min.js'
+		]
+		)
+		.pipe(concat('lib.min.js'))
+		.pipe(gulp.dest(`./${dist_folder_name}/js/`));
+});
 
-	handlePage('./public/pages/gw/welcome.html', `./${dist_folder_name}/pages/gw/`);
-	handlePage('./public/pages/gw/signin.html', `./${dist_folder_name}/pages/gw/`);
-	handlePage('./public/pages/gw/logged-in-home.html', `./${dist_folder_name}/pages/gw/`);
-	handlePage('./public/pages/customer_auth/register.html', `./${dist_folder_name}/pages/customer_auth/`);
-	handlePage('./public/pages/beame_auth/signup.html', `./${dist_folder_name}/pages/beame_auth/`);
+gulp.task('compile-pages', () => {
+
+	compilePage('./public/pages/gw/welcome.html', `./${dist_folder_name}/pages/gw/`);
+	compilePage('./public/pages/gw/signin.html', `./${dist_folder_name}/pages/gw/`);
+	compilePage('./public/pages/gw/logged-in-home.html', `./${dist_folder_name}/pages/gw/`);
+	compilePage('./public/pages/customer_auth/register.html', `./${dist_folder_name}/pages/customer_auth/`);
+	compilePage('./public/pages/beame_auth/signup.html', `./${dist_folder_name}/pages/beame_auth/`);
+});
 
 
-	gulp.src('./public/js/utils.js').pipe(gulp.dest(`./${dist_folder_name}/js/`));
+gulp.task('compile', ['sass', 'compile-css', 'compile-js', 'compile-pages'], () => {
+
 	gulp.src('./public/img/**/*').pipe(gulp.dest(`./${dist_folder_name}/img/`));
-	gulp.src('./public/lib/**/*').pipe(gulp.dest(`./${dist_folder_name}/lib/`));
 	gulp.src('./public/templates/**/*').pipe(gulp.dest(`./${dist_folder_name}/templates/`));
 
 
 });
 
+gulp.task('deploy', ['compile'], function () {
+	var options = {headers: {'Cache-Control': 'max-age=315360000, no-transform, public'}, gzippedOnly: true};
+
+	var config = require('./local_config/aws_config.json');
+
+	var key = config.aws_key, secret = config.aws_secret;
+
+	var aws = {
+		"key":    key,
+		"secret": secret,
+		"bucket": config.bucket,
+		"region": "us-east-1"
+	};
+
+	gulp.src([`./${dist_folder_name}/js/app.min.js`])
+		.pipe(rename("insta-server/js/app.min.js"))
+		.pipe(gzip())
+		.pipe(s3(aws, options));
+
+	gulp.src([`./${dist_folder_name}/js/lib.min.js`])
+		.pipe(rename("insta-server/js/lib.min.js"))
+		.pipe(gzip())
+		.pipe(s3(aws, options));
+
+	gulp.src([`./${dist_folder_name}/js/signin.min.js`])
+		.pipe(rename("insta-server/js/signin.min.js"))
+		.pipe(gzip())
+		.pipe(s3(aws, options));
+
+	gulp.src([`./${dist_folder_name}/js/signup.min.js`])
+		.pipe(rename("insta-server/js/signup.min.js"))
+		.pipe(gzip())
+		.pipe(s3(aws, options));
+
+	gulp.src([`./${dist_folder_name}/css/app.min.css`])
+		.pipe(rename("insta-server/css/app.min.css"))
+		.pipe(gzip())
+		.pipe(s3(aws, options));
+
+
+	setTimeout(function () {
+		var cf_settings = {
+			distribution:    config.beame_cdn_distribution, // Cloudfront distribution ID
+			paths:           ['/insta-server/*'],                 // Paths to invalidate
+			accessKeyId:     key,               // AWS Access Key ID
+			secretAccessKey: secret,        // AWS Secret Access Key
+			wait:            false                     // Whether to wait until invalidation is completed (default: false)
+		};
+
+		gulp.src('*')
+			.pipe(cloudfront(cf_settings));
+
+	}, 1000 * 10);
+
+});
