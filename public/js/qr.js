475071d9c47302c9b3b210acdc357f158299fdab
zenit1 <serge@beame.io>
2016-11-23 16:59:33 +0200

    spinner added to qr

--- public/js/qr.js
+++ public/js/qr.js
@@ -11,13 +11,12 @@ var QrTMPsocketRelay;
 var QrTMPsocketOrigin;
 var qrTmpSocketID;
 var qrRelayEndpoint = "";
-var qrContainer   = null;
-var qrSession = null;
-var matchingFqdn = null;
-var serviceName = null;
+var qrContainer     = null;
+var qrSession       = null;
+var matchingFqdn    = null;
+var serviceName     = null;
 
 $(document).ready(function () {
-
 
 
 	var UID = generateUID(24) + VirtualPrefix;
@@ -27,43 +26,43 @@ $(document).ready(function () {
 	var socket = io.connect("/qr", socketio_options);
 	console.log('QR window ready');
 	socket.on('connect', function () {
-		console.log('QR socket connected, ',qrRelayEndpoint);
+		console.log('QR socket connected, ', qrRelayEndpoint);
 		QrTMPsocketOrigin = socket;//remove towards prod
 		if (!qrRelayEndpoint) {
 			socket.emit('browser_connected', UID);
 		}
 	});
 	socket.on('connect_failed', function () {
-		socket.emit('ack','connect_failed');
-		console.log('QR socket connect_failed,',qrRelayEndpoint);
+		socket.emit('ack', 'connect_failed');
+		console.log('QR socket connect_failed,', qrRelayEndpoint);
 		QrTMPsocketOrigin = socket;//remove towards prod
 		if (!qrRelayEndpoint) {
 			socket.emit('browser_connected', UID);
 		}
 	});
 	socket.on('edgeError', function (data) {
-		socket.emit('ack','edgeError');
+		socket.emit('ack', 'edgeError');
 		console.log('Session failed from server. Network issue.');
 	});
 
-	socket.on('startQrSession',function (data) {
-		if(data){
+	socket.on('startQrSession', function (data) {
+		if (data) {
 			matchingFqdn = data.matching || matchingFqdn;
 			serviceName  = data.service || serviceName;
 		}
-		socket.emit('ack','startQrSession');
+		socket.emit('ack', 'startQrSession');
 		socket.emit('pinRequest');
-		if(!qrSession){
+		if (!qrSession) {
 			qrSession = setInterval(function () {
 				console.log('QR requesting data');
 				socket.emit('pinRequest');
-			},data.refresh_rate);
+			}, data.refresh_rate);
 		}
 	});
 
 	socket.on('pinRenew', function (data) {
-		socket.emit('ack','pinRenew');
-		console.log('pinRenew:',data);
+		socket.emit('ack', 'pinRenew');
+		console.log('pinRenew:', data);
 		if (stopAllRunningSessions) {
 			console.log('QR session stopped from server');
 			resetQR();
@@ -84,21 +83,21 @@ $(document).ready(function () {
 								window.alert('Warning! Suspicious content, please verify domain URL and reload the page..');
 							}
 							else {
-								var qrType = (auth_mode == "Provision")?"PROV":"LOGIN";
-								var QRdata       = {
-									'relay': 'https://' + qrRelayEndpoint,
-									'PK': PK,
-									'UID': parsed['UID'],
-									'PIN':   parsed['data'],
-									'TYPE': qrType,
-									'SOCKET_OPT' : socketio_options ,
-									'TIME': Date.now(),
-									'REG': reg_data || 'login',
-									'matching': matchingFqdn,
-									'service' : serviceName
+								var qrType = (auth_mode == "Provision") ? "PROV" : "LOGIN";
+								var QRdata = {
+									'relay':      'https://' + qrRelayEndpoint,
+									'PK':         PK,
+									'UID':        parsed['UID'],
+									'PIN':        parsed['data'],
+									'TYPE':       qrType,
+									'SOCKET_OPT': socketio_options,
+									'TIME':       Date.now(),
+									'REG':        reg_data || 'login',
+									'matching':   matchingFqdn,
+									'service':    serviceName
 
 								};
-								console.log('QR DATA:',QRdata);
+								console.log('QR DATA:', QRdata);
 								socket.emit('QRdata', QRdata);
 								qrContainer = $('#qr');
 								try {
@@ -107,12 +106,14 @@ $(document).ready(function () {
 										var qrCode = dataStr;
 										console.log(qrCode);
 										qrContainer.empty();
+										qrContainer.removeClass('qr-spinner');
 										qrContainer.kendoQRCode({
 											value:           qrCode,
 											errorCorrection: "L",
 											color:           "#000",
 											background:      "transparent",
-											padding:         0
+											padding:         0,
+											size:            220
 										});
 									}
 									else {
@@ -142,8 +143,8 @@ $(document).ready(function () {
 	});
 
 	socket.on('mobileProv1', function (data) {
-		socket.emit('ack','mobileProv1');
-		console.log('QR mobileProv1:',data);
+		socket.emit('ack', 'mobileProv1');
+		console.log('QR mobileProv1:', data);
 		if (data.data && QrTMPsocketRelay) {
 			var msg = {'socketId': qrTmpSocketID, 'payload': JSON.stringify(data)};
 			console.log('QR ******** Sedning:: ', msg);
@@ -152,7 +153,7 @@ $(document).ready(function () {
 	});
 
 	socket.on('mobilePinInvalid', function (data) {
-		socket.emit('ack','mobilePinInvalid');
+		socket.emit('ack', 'mobilePinInvalid');
 		console.log('QR ***mobilePinInvalid***** Sedning:: ', msg);
 		if (data.data && QrTMPsocketRelay) {
 			var msg = {'socketId': qrTmpSocketID, 'payload': JSON.stringify(data)};
@@ -162,19 +163,19 @@ $(document).ready(function () {
 	});
 
 	socket.on('relayEndpoint', function (data) {
-		socket.emit('ack','relayEndpoint');
+		socket.emit('ack', 'relayEndpoint');
 		console.log('QR relayEndpoint', data);
 		generateKeyPairs(function (error, keydata) {
 			if (error) return;//send error to origin/show on browser
-			if(!keyGenerated){
-				keyPair     = keydata.keyPair;
-				keyPairSign = keydata.keyPairSign;
+			if (!keyGenerated) {
+				keyPair      = keydata.keyPair;
+				keyPairSign  = keydata.keyPairSign;
 				keyGenerated = true;
 			}
 			try {
-				var parsedData = JSON.parse(data);
-				qrRelayEndpoint  = parsedData['data'];
-				var lclTarget  = "https://" + qrRelayEndpoint + "/control";
+				var parsedData  = JSON.parse(data);
+				qrRelayEndpoint = parsedData['data'];
+				var lclTarget   = "https://" + qrRelayEndpoint + "/control";
 				if (qrRelayEndpoint) {
 					//noinspection ES6ModulesDependencies,NodeModulesDependencies
 					QrTMPsocketRelay = io.connect(lclTarget);
@@ -210,23 +211,25 @@ $(document).ready(function () {
 	});
 
 	var resetQR = function () {
-		if(!qrContainer)return;
-		if(qrSession)clearInterval(qrSession);
+		if (!qrContainer)return;
+		if (qrSession) clearInterval(qrSession);
 		socket.emit('close_session');
 		console.log('QR read successfully - set green');
 		qrContainer.empty();
+		qrContainer.removeClass('qr-spinner');
 		//noinspection JSUnresolvedFunction
 		qrContainer.kendoQRCode({
 			value:           "{\"message\":\"QR used, reload the page to get new QR\"}",
 			errorCorrection: "L",
 			color:           "#0F9239",
 			background:      "transparent",
-			padding:         0
+			padding:         0,
+			size:            220
 		});
 	};
 
 	socket.on('resetQR', function () {
-		socket.emit('ack','resetQR');
+		socket.emit('ack', 'resetQR');
 		console.log('QR resetQR');
 		stopAllRunningSessions = true;
 		resetQR();
@@ -251,7 +254,7 @@ function initRelay(socket) {
 	QrTMPsocketRelay.on('data', function (data) {
 		console.log('QR relay data');
 		qrTmpSocketID = data.socketId;
-		processMobileData(QrTMPsocketRelay,QrTMPsocketOrigin, data);
+		processMobileData(QrTMPsocketRelay, QrTMPsocketOrigin, data);
 		QrTMPsocketRelay.beame_relay_socket_id = data.socketId;
 
 	});
