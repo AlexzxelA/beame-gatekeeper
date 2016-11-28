6774f2766cbc34c502be84e248bed28c1971a36a
zenit1 <serge@beame.io>
2016-11-28 20:15:09 +0200

    test photos in frame completed

--- apps/photo/public/js/app.js
+++ apps/photo/public/js/app.js
@@ -9,34 +9,37 @@ function loadImage(url) {
 
 	loader.style.visibility='visible';
 
-	var img = new Image();
+	var tempImage = new Image();
 
 	function notify_complete() {
 		loader.style.visibility='hidden';
+		console.log('set url to ' + url);
 		img.src  = url;
+		img.style.visibility='visible';
 	}
 
-	img.onload = notify_complete;
-	img.src    = url;
+	tempImage.onload = notify_complete;
+	tempImage.src    = url;
+}
+
+function onUrlReceived(args){
+	if (!args || !args.url) {
+		return;
+	}
+
+	var url = args.url + '?w=600&h=600';
+
+	img    = document.getElementById('img-photo');
+	loader = document.getElementById('img-loader');
+
+	loadImage(url);
 }
 
 function onDocLoaded() {
 
 	window.parent.document.title = 'Mobile Photos';
 
-	window.parent.getNotifManagerInstance().subscribe('MOBILE_PHOTO_URL', function (event, args) {
-		if (!args || !args.url) {
-			return;
-		}
-
-		var url = args.url + '?w=600&h=600';
-
-		img    = document.getElementById('img-photo');
-		loader = document.getElementById('img-loader');
-
-		loadImage(url);
-
-	});
+	window.parent.getNotifManagerInstance().subscribe('MOBILE_PHOTO_URL', onUrlReceived);
 }
 
 
