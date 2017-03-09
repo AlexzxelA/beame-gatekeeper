efd5113a007e1489fadbfb8dfd68be78ed780b71
Ilya Sher <ilya.sher@coding-knight.com>
2017-03-09 20:46:30 +0200

    Fix runSequilizeCmd() call -- directories locations

--- src/bootstrapper.js
+++ src/bootstrapper.js
@@ -807,6 +807,16 @@ class Bootstrapper {
 	//endregion
 
 	//region init sqlite db
+
+	_commonSequelizeArgs() {
+		let result = [];
+		['migrations', 'seeders', 'models'].forEach(what => {
+			result.push(`--${what}-path`);
+			result.push(path.join(path.dirname(__dirname), what));
+		});
+		return result;
+	}
+
 	_migrateSqliteSchema() {
 
 		logger.debug(`migrating sqlite schema...`);
@@ -814,6 +824,7 @@ class Bootstrapper {
 		return new Promise((resolve, reject) => {
 				//TODO implement https://github.com/sequelize/umzug
 				let args = ["db:migrate", "--env", this._config[SqliteProps.EnvName], "--config", SqliteConfigJsonPath];
+				args.splice.apply(args, [1, 0].concat(this._commonSequelizeArgs()));
 
 				CommonUtils.runSequilizeCmd(require.resolve('sequelize'), args).then(()=>{
 					logger.debug(`sqlite migration completed successfully...`);
@@ -830,6 +841,7 @@ class Bootstrapper {
 
 		return new Promise((resolve, reject) => {
 				let args = ["db:seed:all", "--env", this._config[SqliteProps.EnvName], "--config", SqliteConfigJsonPath];
+				args.splice.apply(args, [1, 0].concat(this._commonSequelizeArgs()));
 
 				CommonUtils.runSequilizeCmd(require.resolve('sequelize'), args).then(()=>{
 					logger.debug(`sqlite seeders applied successfully...`);
